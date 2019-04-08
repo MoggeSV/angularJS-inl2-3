2d1399631668753afd54bd0b04611f0117d9b478
Morgan Svensson <morgan.svensson10@gmail.comgit config --global user.email>
2019-04-08 10:39:48 +0200

    Uppgift 2-3 redo

--- controllers/angularController.js
+++ controllers/angularController.js
@@ -1,5 +1,36 @@
-app.controller("angularController", function($scope) {
+var app = angular
+    .module("angularApp", ["ngRoute"])
     
-    $scope.products = products;
+    .config(function($routeProvider) {
+        $routeProvider
+        .when("/", { 
+            templateUrl: "views/grid.view.html",
+            controller: "angularController"
+    
+        })
 
-});
+        .when("/gridview", { 
+            templateUrl: "views/grid.view.html",
+            controller: "angularController"
+    
+        })
+
+        .when("/listview", { 
+            templateUrl: "views/list.view.html",
+            controller: "angularController"
+    
+        })
+    
+    })
+    
+    .controller("angularController", function($rootScope) {
+        
+        $rootScope.products = products;
+    
+        $rootScope.rowLimit = {
+            "type": "select",
+            "name": "rowLimit",
+            "value": "5",
+            "values": ["5","10","20", "50", "100"]
+        };
+    })
